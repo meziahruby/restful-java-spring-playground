977d58e07304fab091550b3cad0b604ca69c0ab8
Meziah Cristobal <mezcrist@cisco.com>
2019-10-01 11:33:43 -0700

    Quick start with Spring Initialzr

---
+++ udemy-spring-basics/src/main/java/com/playground/spring/udemyspringbasics/UdemySpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.playground.spring.udemyspringbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringBasicsApplication.class, args);
+	}
+
+}
